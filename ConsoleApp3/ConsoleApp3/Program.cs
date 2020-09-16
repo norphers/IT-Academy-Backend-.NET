9abf7c310ede5120b7005c7c2dafe8e0d26f31dc
ocanades <ocanades@gmail.com>
2020-09-16 14:39:54 +0200

    initial commit .NET Core

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Stage1()
+        {
+
+        }
+
+        static void Stage2()
+        {
+
+        }
+
+        static void Stage3()
+        {
+
+        }
+
+        static void Stage4()
+        {
+
+        }
+
+    }
+}
