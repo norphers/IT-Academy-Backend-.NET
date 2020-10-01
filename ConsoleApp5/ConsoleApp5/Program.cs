ec9e4631902cb8b0e99c653dd691763d43803e09
ocanades <ocanades@gmail.com>
2020-10-01 09:59:55 +0200

    exer-5 finished

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -8,17 +8,19 @@ namespace ConsoleApp5
     class Program
     {
         static int[] serie1 = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+        static string[] names = { "David", "Sergio", "Maria", "Laura", "Oscar", "Julia", "Oriol" };
         static void Main(string[] args)
         {
             Stage1();
             Stage2();
             Stage3();
+            Stage4();
         }
 
         static void Stage1()
         {
             Console.WriteLine("Stage 1.");
-            PrintArray(PairArrayConstructor(GetPairs(serie1)));
+            PrintIntArray(PairArrayConstructor(GetPairs(serie1)));
         }
 
         static void Stage2()
@@ -33,9 +35,20 @@ namespace ConsoleApp5
         {
             Console.WriteLine("Stage 3.");
             Console.Write("Numbers bigger than 5: ");
-            PrintArray(NumbersBiggerThan5(serie1));
+            PrintIntArray(NumbersBiggerThan5(serie1));
             Console.Write("Numbers lower than 5: ");
-            PrintArray(NumbersLowerThan5(serie1));
+            PrintIntArray(NumbersLowerThan5(serie1));
+        }
+
+        static void Stage4()
+        {
+            Console.WriteLine("Stage 4.");
+            Console.Write("Names starting with 'O': ");
+            PrintStringArray(NamesStartingO(names));
+            Console.Write("Names equal or bigger than 6 characters: ");
+            PrintStringArray(NamesLength(names));
+            Console.Write("Array names sorted reverse: ");
+            PrintStringArray(NamesSortReverse(names));
         }
 
         static IEnumerable<int> GetPairs(int[] array)
@@ -55,9 +68,15 @@ namespace ConsoleApp5
             return pairArray;
         }
 
-        public static void PrintArray(int[] array)
+        public static void PrintIntArray(int[] array)
         {
             foreach (int i in array) Console.Write(i + " ");
+            Console.WriteLine("");
+        }
+
+        public static void PrintStringArray(string[] array)
+        {
+            foreach (string s in array) Console.Write(s + " ");
             Console.WriteLine("");
         }
 
@@ -83,11 +102,7 @@ namespace ConsoleApp5
         {
             IEnumerable<int> biggerThan5Serie = (from number in array where number>5 select number);
             int[] biggerThan5Array = new int[biggerThan5Serie.Count()];
-
-            for (var i = 0; i < biggerThan5Serie.Count(); i++)
-            {
-                biggerThan5Array[i] = biggerThan5Serie.ElementAt(i);
-            }
+            for (var i = 0; i < biggerThan5Serie.Count(); i++) biggerThan5Array[i] = biggerThan5Serie.ElementAt(i);
             return biggerThan5Array;
         }
 
@@ -95,21 +110,33 @@ namespace ConsoleApp5
         {
             IEnumerable<int> lowerThan5Serie = (from number in array where number <= 5 select number);
             int[] lowerThan5Array = new int[lowerThan5Serie.Count()];
-
-            for (var i = 0; i < lowerThan5Serie.Count(); i++)
-            {
-                lowerThan5Array[i] = lowerThan5Serie.ElementAt(i);
-            }
+            for (var i = 0; i < lowerThan5Serie.Count(); i++) lowerThan5Array[i] = lowerThan5Serie.ElementAt(i);
             return lowerThan5Array;
         }
 
+        static string[] NamesStartingO(string[] array)
+        {
+            IEnumerable<string> names = (from name in array where name.StartsWith("o") || name.StartsWith("O") select name);
+            string[] namesStartingO = new string[names.Count()];
+            for (var i = 0; i < names.Count(); i++) namesStartingO[i] = names.ElementAt(i);
+            return namesStartingO;
+        }
 
-        /*
-         * En aquesta fase farem servir un array amb noms: (David, Sergio, Maria, Laura, Oscar, Julia, Oriol)
-            Creeu un array amb els noms de l’enunciat. (0,5 punts)
-            Fent servir consultes linq mostreu per consola els noms que comencin amb la lletra “O”. (1 punt)
-            Fent servir consultes linq mostreu per consola quins noms tenen més de 6 lletres. (1 punt)
-            Fent servir consultes linq mostreu per consola els noms de l’array ordenats de manera descendent. 
-        */
+        static string[] NamesLength(string[] array)
+        {
+            IEnumerable<string> names = (from name in array where name.Length>=6 select name);
+            string[] namesLength = new string[names.Count()];
+            for (var i = 0; i < names.Count(); i++) namesLength[i] = names.ElementAt(i);
+            return namesLength;
+        }
+
+        static string[] NamesSortReverse(string[] array)
+        {
+            IEnumerable<string> names = (from name in array orderby name select name).Reverse();
+            string[] namesSortReverse = new string[names.Count()];
+            for (var i = 0; i < names.Count(); i++) namesSortReverse[i] = names.ElementAt(i);
+            return namesSortReverse;
+        }
+
     }
 }
